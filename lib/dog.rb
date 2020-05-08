a1c899c292f4851f4657a11fad59410df94ae8e3
Toni  Diep <adiep123@gmail.com>
2020-05-08 04:31:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
-  attr_accessor :name :breed
+  # attr_accessor :name :breed
 
-# def initialize(name)
-#   @name = name
-# end
+def initialize(breed)
+  @breed = breed
+end
 
 # def name 
 #   @name 
